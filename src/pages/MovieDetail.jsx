9651193cdd53dbb759b02b8922cffc5de82d69b6
DANUSHKA\danus <danushkasomasiri@gmail.com>
2025-05-15 10:39:03 +0200

    movie details page setup and medeai queries

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Loader from '../components/Loader';
-import ErrorMessage from '../components/ErrorMessage';
-import { useNavigate } from 'react-router-dom';
-import NotFound from './NotFound';
-import styled from 'styled-components'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Loader from "../components/Loader";
+import ErrorMessage from "../components/ErrorMessage";
+import { useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
+import styled from "styled-components";
 
 // import ErrorMessage from '../components/ErrorMessage';
-
-
 
 const Backdrop = styled.div`
   min-height: 100vh;
@@ -17,6 +15,35 @@ const Backdrop = styled.div`
   background-position: center;
   position: relative;
   color: white;
+  background-image: ${({ backdrop }) =>
+    `url(https://image.tmdb.org/t/p/original${backdrop})`};
+
+  &::before {
+    content: "";
+    position: absolute;
+    inset: 0;
+    background: linear-gradient(to top, rgba(8, 8, 8, 1), rgba(14, 14, 14, 0));
+    z-index: 0;
+  }
+
+  > * {
+    position: relative;
+    z-index: 1;
+  }
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  content: "";
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: linear-gradient(to top, rgba(8, 8, 8, 1), rgba(14, 14, 14, 0));
 `;
 
 const BackButton = styled.button`
@@ -35,17 +62,53 @@ const BackButton = styled.button`
   }
 `;
 
-const Poster = styled.img`
-  width: 100%;    
-  max-width: 300px;
- border:5px solid white;
-  margin: 15rem 0 0 5rem;
+const MovieDetailsCon = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  padding: 2rem;
 
- 
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: flex-end;
+    padding: 3rem;
+  }
 `;
 
+const Poster = styled.img`
+  max-width: 80%;
+  border: 5px solid white;
+  /* margin: 15rem 0 0 5rem; */
 
+  @media (min-width: 768px) {
+    width: 35%;
+  }
 
+  @media (min-width: 1024px) {
+    max-width: 350px;
+  }
+`;
+
+const MovieDetailstext = styled.div`
+  gap: 1rem;
+  display: flex;
+  flex-direction: column;
+
+  p {
+    max-width: 70ch;
+  }
+`;
+
+const MovieTitle = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  h3 {
+    background-color: rgba(12, 12, 12, 0.2);
+    padding: 0.5rem;
+  }
+`;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const MovieDetail = () => {
@@ -66,7 +129,7 @@ const MovieDetail = () => {
     )
       .then((res) => {
         if (!res.ok) {
-          throw new Error('Failed to fetch movie details');
+          throw new Error("Failed to fetch movie details");
         }
         return res.json();
       })
@@ -95,23 +158,28 @@ const MovieDetail = () => {
   }
 
   const handleBack = () => {
-    navigate('/');
+    navigate("/");
   };
   // Om allt är OK, rendera startsidan
 
   return (
-    <Backdrop style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})` }}  >
+    <Backdrop backdrop={movie.backdrop_path}>
       <BackButton onClick={handleBack}>◀ Movies</BackButton>
 
+      <MovieDetailsCon>
+        <Poster
+          src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
+          alt={movie.title}
+          loading="lazy"
+        />
 
-<Poster src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`} alt={movie.title}  loading='lazy' />
-      <h1>{movie.title}</h1>
-  
-      <p>{movie.overview}</p>
-      <p>⭐ {movie.vote_average}</p>
-
-
-
+        <MovieDetailstext>
+          <MovieTitle>
+            <h2>{movie.title}</h2> <h3>⭐ {movie.vote_average}</h3>
+          </MovieTitle>
+          <p>{movie.overview}</p>
+        </MovieDetailstext>
+      </MovieDetailsCon>
     </Backdrop>
   );
 };
